4871ec08f122278443d63ec9223d3530843ea854
Lily Scott <git@suchipi.tv>
2022-03-01 14:13:51 -0700

    Export individual parts of defaultLoader so it's easier to override it without losing its usefulness

--- src/default-loader.ts
+++ src/default-loader.ts
@@ -6,82 +6,113 @@ import makeDebug from "debug";
 
 const debug = makeDebug("kame/default-loader");
 
+export function stripShebang(content: string) {
+  return content.replace(/^#![^\r\n]*/, "");
+}
+
+export function loadJson(filename: string) {
+  debug(`loadJson`);
+
+  return "module.exports = " + fs.readFileSync(filename, "utf-8");
+}
+
+export function loadCss(filename: string) {
+  debug(`loadCss`);
+
+  const content = fs.readFileSync(filename, "utf-8");
+  return `
+    var style = document.createElement("style");
+    style.type = "text/css";
+    style.textContent = ${JSON.stringify(content)};
+    document.head.appendChild(style);
+  `;
+}
+
+export function loadJsUncompiled(filename: string) {
+  debug(`loadJsUncompiled`);
+  const code = fs.readFileSync(filename, "utf-8");
+  return stripShebang(code);
+}
+
+export function loadJsCompiled(
+  filename: string,
+  babelEnvOptions: { [key: string]: any } = {}
+) {
+  debug(`loadJsCompiled`);
+  const extension = path.extname(filename);
+
+  const config: babel.TransformOptions = {
+    babelrc: false,
+    sourceType: "unambiguous",
+    presets: [
+      [
+        require("@babel/preset-env").default,
+        { modules: false, ...babelEnvOptions },
+      ],
+      require("@babel/preset-react").default,
+    ],
+    plugins: [
+      require("@babel/plugin-proposal-class-properties").default,
+      require("@babel/plugin-proposal-nullish-coalescing-operator").default,
+      require("@babel/plugin-proposal-optional-chaining").default,
+      require("@babel/plugin-transform-modules-commonjs").default,
+    ],
+    filename,
+  };
+
+  if (extension === ".ts" || extension === ".tsx") {
+    config.presets!.push(require("@babel/preset-typescript").default);
+  } else {
+    config.plugins!.push(
+      require("@babel/plugin-transform-flow-strip-types").default
+    );
+  }
+
+  const result = babel.transformFileSync(filename, config);
+  const code = result?.code || "";
+
+  return stripShebang(code);
+}
+
+export function loadFile(filename: string) {
+  debug(`loadFile`);
+
+  const extension = path.extname(filename);
+
+  const type = mime.lookup(extension) || "application/octet-stream";
+  const base64 = fs.readFileSync(filename, "base64");
+  const url = `data:${type};base64,${base64}`;
+  return `module.exports = ${JSON.stringify(url)}`;
+}
+
 function defaultLoader(
   filename: string,
   babelEnvOptions: { [key: string]: any } = {}
 ): string {
   debug(`Loading ${filename}`);
-
   const extension = path.extname(filename);
 
   switch (extension) {
     case ".json": {
-      return "module.exports = " + fs.readFileSync(filename, "utf-8");
+      return loadJson(filename);
     }
     case ".css": {
-      const content = fs.readFileSync(filename, "utf-8");
-      return `
-        var style = document.createElement("style");
-        style.type = "text/css";
-        style.textContent = ${JSON.stringify(content)};
-        document.head.appendChild(style);
-      `;
+      return loadCss(filename);
     }
     case ".js":
     case ".jsx":
     case ".mjs":
     case ".ts":
     case ".tsx": {
-      debug(`js case`);
-
-      let code = "";
       if (extension === ".js" && filename.match(/node_modules/)) {
-        debug(`js case uncompiled`);
-        code = fs.readFileSync(filename, "utf-8");
+        return loadJsUncompiled(filename);
       } else {
-        const config: babel.TransformOptions = {
-          babelrc: false,
-          sourceType: "unambiguous" as "unambiguous",
-          presets: [
-            [
-              require("@babel/preset-env").default,
-              { modules: false, ...babelEnvOptions },
-            ],
-            require("@babel/preset-react").default,
-          ],
-          plugins: [
-            require("@babel/plugin-proposal-class-properties").default,
-            require("@babel/plugin-proposal-nullish-coalescing-operator")
-              .default,
-            require("@babel/plugin-proposal-optional-chaining").default,
-            require("@babel/plugin-transform-modules-commonjs").default,
-          ],
-          filename,
-        };
-
-        if (extension === ".ts" || extension === ".tsx") {
-          config.presets!.push(require("@babel/preset-typescript").default);
-        } else {
-          config.plugins!.push(
-            require("@babel/plugin-transform-flow-strip-types").default
-          );
-        }
-
-        const result = babel.transformFileSync(filename, config);
-        debug(`js case compiled`);
-        code = result?.code || "";
+        return loadJsCompiled(filename, babelEnvOptions);
       }
-
-      const codeWithoutShebang = code.replace(/^#![^\r\n]*/, "");
-      return codeWithoutShebang;
     }
 
     default: {
-      debug(`default case`);
-      const type = mime.lookup(extension) || "application/octet-stream";
-      const base64 = fs.readFileSync(filename, "base64");
-      const url = `data:${type};base64,${base64}`;
-      return `module.exports = ${JSON.stringify(url)}`;
+      return loadFile(filename);
     }
   }
 }
