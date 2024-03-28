5d116fb24228484631132a56932dcc4b2fde7ded
Lily Skye <me@suchipi.com>
2024-03-28 01:11:58 -0600

    use @suchipi/error-utils (which was based on source-maps.ts from this repo)

--- cli-tests/runtime-error-sourcemaps/index.test.ts
+++ cli-tests/runtime-error-sourcemaps/index.test.ts
@@ -17,17 +17,17 @@ test("works", async () => {
                     throw (0, source_maps_1.applySourceMapsToError)(sourceMaps, err);
                     ^
 
-    [Error: uh oh spongebob
-      at Whatever.someOtherThirdThing (<rootDir>/cli-tests/runtime-error-sourcemaps/index.ts:21:17)
-      at Whatever.anotherMethod (<rootDir>/cli-tests/runtime-error-sourcemaps/index.ts:17:10)
-      at Whatever.someMethod (<rootDir>/cli-tests/runtime-error-sourcemaps/index.ts:13:10)
-      at new Whatever (<rootDir>/cli-tests/runtime-error-sourcemaps/index.ts:9:10)
-      at <rootDir>/cli-tests/runtime-error-sourcemaps/index.ts:28:18
-      at Object.run (<rootDir>/dist/runtime.js:118:13)
-      at Module._load (<rootDir>/node_modules/commonjs-standalone/dist/index.js:43:22)
-      at Runtime._run (<rootDir>/dist/runtime.js:134:53)
-      at Runtime.load (<rootDir>/dist/runtime.js:130:25)
-      at Object.<anonymous> (<rootDir>/dist/cli/cmd-run.js:18:9)] {
+    Error: uh oh spongebob
+        at Whatever.someOtherThirdThing (<rootDir>/cli-tests/runtime-error-sourcemaps/index.ts:21:17)
+        at Whatever.anotherMethod (<rootDir>/cli-tests/runtime-error-sourcemaps/index.ts:17:10)
+        at Whatever.someMethod (<rootDir>/cli-tests/runtime-error-sourcemaps/index.ts:13:10)
+        at new Whatever (<rootDir>/cli-tests/runtime-error-sourcemaps/index.ts:9:10)
+        at <rootDir>/cli-tests/runtime-error-sourcemaps/index.ts:28:18
+        at Object.run (<rootDir>/dist/runtime.js:118:13)
+        at Module._load (<rootDir>/node_modules/commonjs-standalone/dist/index.js:43:22)
+        at Runtime._run (<rootDir>/dist/runtime.js:134:53)
+        at Runtime.load (<rootDir>/dist/runtime.js:130:25)
+        at Object.<anonymous> (<rootDir>/dist/cli/cmd-run.js:18:9) {
       someProperty: 65
     }
 
--- src/source-maps.ts
+++ src/source-maps.ts
@@ -1,125 +1,34 @@
 import util from "util";
-import * as pheno from "pheno";
-import ErrorStackParser from "error-stack-parser";
-import { SourceMapConsumer } from "source-map";
+import {
+  ParsedError,
+  applySourceMapsToParsedError,
+  SourceMap,
+  isError,
+} from "@suchipi/error-utils";
 
-export type SourceMap = {
-  /**
-   * Which version of the source map spec this map is following.
-   */
-  version: number;
-
-  /**
-   * An array of URLs to the original source files.
-   */
-  sources: Array<string>;
-
-  /**
-   * An array of identifiers which can be referenced by individual mappings.
-   */
-  names: Array<string>;
-
-  /**
-   * Optional. The URL root from which all sources are relative.
-   */
-  sourceRoot?: string;
-
-  /**
-   * Optional. An array of contents of the original source files.
-   */
-  sourcesContent?: Array<string>;
-
-  /**
-   * A string of base64 VLQs which contain the actual mappings.
-   */
-  mappings: string;
-
-  /**
-   * Optional. The generated filename this source map is associated with.
-   */
-  file?: string;
-};
+export type { SourceMap };
 
 export function applySourceMapsToError(
   sourceMaps: { [key: string]: SourceMap },
   err: unknown
-) {
+): Error {
   let error: Error;
-  if (!pheno.isOfType(err, pheno.Error)) {
+  if (!isError(err)) {
     error = new Error("Non-error value was thrown: " + util.inspect(err));
   } else {
     error = err;
   }
 
-  const stackFrames = ErrorStackParser.parse(error);
+  const parsed = new ParsedError(error);
+  const mapped = applySourceMapsToParsedError(sourceMaps, parsed);
 
-  const newFrameLines: Array<string> = [];
-
-  for (const frame of stackFrames) {
-    let output = "  at ";
-
-    if (frame.isConstructor) {
-      output += "new ";
-    }
-
-    let fileNameShouldBeWrappedInParens = false;
-    if (frame.functionName) {
-      output += frame.functionName;
-      output += " ";
-
-      fileNameShouldBeWrappedInParens = true;
-    }
-
-    if (frame.fileName) {
-      if (fileNameShouldBeWrappedInParens) {
-        output += "(";
-      }
-
-      output += frame.fileName;
-
-      if (frame.lineNumber) {
-        let line = frame.lineNumber;
-        let column = frame.columnNumber;
-
-        if (sourceMaps[frame.fileName] != null) {
-          const map = sourceMaps[frame.fileName];
-
-          const consumer = new SourceMapConsumer(map as any);
-
-          const pos = consumer.originalPositionFor({
-            line: frame.lineNumber || 1,
-            column: frame.columnNumber || 0,
-          });
-
-          line = pos.line;
-          column = pos.column + 1;
-        }
-
-        output += ":";
-        output += line;
-
-        if (frame.columnNumber) {
-          output += ":";
-          output += column;
-        }
-      }
-
-      if (fileNameShouldBeWrappedInParens) {
-        output += ")";
-      }
-    }
-
-    newFrameLines.push(output);
-  }
-
-  Object.defineProperty(error, "stack", {
-    value: [
-      error.name,
-      ": ",
-      error.message,
-      "\n",
-      newFrameLines.join("\n"),
-    ].join(""),
+  Object.defineProperties(error, {
+    stack: {
+      value: mapped.stack,
+      writable: true,
+      enumerable: false,
+      configurable: true,
+    },
   });
 
   return error;
