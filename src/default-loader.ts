7dbe576a66b653c5d94125cd1f2950e643a26396
Lily Skye <git@suchipi.tv>
2026-06-16 19:56:39 -0600

    ditch mlly
    it was incorrectly flagging babel's CJS as containing ESM syntax

--- src/default-loader.ts
+++ src/default-loader.ts
@@ -3,7 +3,6 @@ import path from "path";
 import mime from "mime-types";
 import * as swc from "@swc/core";
 import makeDebug from "debug";
-import { hasESMSyntax } from "mlly";
 
 const debug = makeDebug("kame/default-loader");
 
@@ -161,12 +160,7 @@ function defaultLoader(
         (extension === ".js" || extension === ".cjs") &&
         filename.match(/node_modules/)
       ) {
-        const rawCode = loadJsUncompiled(filename);
-        if (hasESMSyntax(rawCode)) {
-          return loadJsCompiled(filename, { target });
-        } else {
-          return rawCode;
-        }
+        return loadJsUncompiled(filename);
       } else {
         return loadJsCompiled(filename, { target });
       }
